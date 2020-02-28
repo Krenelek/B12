d4287e9bc8c2e584cf022f5c979b715b25af3b10
Krenelek <lenrek.mada@gmail.com>
2020-02-28 17:54:27 +0100

    initial commit

---
+++ B12/Cipher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B12
+{
+    class Cipher
+    {
+        public void Showkey()
+        {
+            for (int i=0;i< 24;i++)
+            {
+                if (i % 8 == 0)
+                    Console.WriteLine();
+                Console.Write(key[i]);
+            }
+        }
+        public Cipher()
+        {
+            key = new int[2048];
+            Random rnd = new Random();
+            for (int i=0;i<key.Length;i++)
+            {
+                key[i] = rnd.Next(2);                
+            }
+        }
+        private int[] key;
+        public string CesarCipher(string s, int n)
+        {
+            char[] helper = new char[s.Length];
+            for (int i=0; i < s.Length;i++)
+            {
+                helper[i] = Convert.ToChar(Convert.ToInt32(s[i]) + n % 256);
+            }
+
+            string output = string.Concat(helper);
+            return output;
+
+        }
+        public string XORCipher(string s)
+        {
+            string str;
+            int k = 0;
+            string helper = "";
+            string result = "";
+            int[] binary = new int[s.Length];
+            for (int i =0;i<s.Length;i++)
+            {
+                str = Convert.ToString(s[i], 2).PadLeft(8, '0');
+                for(int j=0;j<str.Length;j++)
+                {
+                    if (((str[j] == '1') ^ (key[k] == 1)))
+                    {
+                        helper = helper+"1";
+                    }
+                    else
+                    {
+                        helper = helper+"0";
+                    }
+                    k++;                    
+                }
+                result = result+ Convert.ToChar(Convert.ToInt32(helper, 2));
+                helper = "";
+            }
+            return result;
+        }    
+    
+ 
+    }
+}
---
+++ B12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a1 = "Tajne haslo to: akustyka";
+            string a2 = "Testowa wiadomosc hakera";
+            Cipher szyfrowanko = new Cipher();
+            Console.WriteLine(szyfrowanko.CesarCipher(a1,3));
+            string b1=szyfrowanko.XORCipher(a1);
+            Console.WriteLine(b1);
+            string b2 = szyfrowanko.XORCipher(a2);
+            string guessed="";
+            Console.WriteLine(b2);
+            for (int i=0;i<a2.Length;i++)
+            {
+                string sum = "";
+                string sb1 = Convert.ToString(b1[i], 2).PadLeft(8, '0');
+                string sb2 = Convert.ToString(b2[i], 2).PadLeft(8, '0');
+                string sa2 = Convert.ToString(a2[i], 2).PadLeft(8, '0');
+                for (int j=0;j<8;j++)
+                {
+                    int bb1 = (Convert.ToInt32(sb1[j]) - 48);
+                    int bb2 = ((Convert.ToInt32(sb2[j])) - 48);
+                    int aa2 = ((Convert.ToInt32(sa2[j])) - 48);
+                    sum = sum + ((bb1^bb2)^aa2).ToString();
+                    Console.WriteLine(bb1 + " " + bb2 + " " + aa2 + " " + ((bb1 ^ bb2) ^ aa2).ToString());
+                }
+                Console.WriteLine(sum);
+
+                Console.WriteLine(Convert.ToChar(Convert.ToInt32(sum, 2)));
+                guessed = guessed + Convert.ToChar(Convert.ToInt32(sum, 2));
+            }
+            Console.WriteLine();
+            Console.WriteLine(guessed);
+        }
+    }
+}
